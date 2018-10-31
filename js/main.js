76aa3d7fba7a37b061c9dda792b48462cb6a3a60
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-10-31 13:52:29 -0700

    took out contact form ajax call

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        // mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
     $WIN = $(window);
@@ -295,6 +295,7 @@
 
    /* AjaxChimp
     * ------------------------------------------------------ */
+   /*
     var clAjaxChimp = function() {
         
         $('#mc-form').ajaxChimp({
@@ -324,6 +325,7 @@
         } 
 
     };
+    */
 
 
    /* Back to Top
@@ -362,7 +364,7 @@
         clPlaceholder();
         clAlertBoxes();
         clAOS();
-        clAjaxChimp();
+        // clAjaxChimp();
         clBackToTop();
 
     })();
